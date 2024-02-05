007149a2cdc45309e69381e415f1298a762f5b1d
emileeM02 <bug0aloo2@gmail.com>
2024-02-05 15:39:04 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # exercise3
+
+"Love is the greatest refreshment in life." - Pablo Picasso
---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = [
+    "You must be the change you wish to see in the world. -Mahatma Gandhi",
+    "Be yourself; everyone else is already taken. -Oscar Wilde",
+    "The way I see it, if you want the rainbow, you gotta put up with the rain. -Dolly Parton"
+]
+
+console.log(quotes[Math.floor(Math.random()*3)])
